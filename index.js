e45c3b08e8369ab81e06ea50a33886f2d35cdca0
Sumanto Banerjee <68064874+banerjee08@users.noreply.github.com>
2023-02-13 12:40:10 +0530

    refactoring

--- index.js
+++ index.js
@@ -4,20 +4,30 @@ const clearCompleted = document.getElementById('clear-completed');
 const deleteBtn = document.getElementById('delete-btn');
 const addTask = document.getElementById('add-task');
 const incomplete = document.getElementById('incomplete');
+const completed = document.getElementById('completed');
 
 // array
 const myTasks = [];
 
 inputAdd.addEventListener('click', function () {
   myTasks.push(addTask.value);
-    console.log(myTasks);
-    
-    incomplete.innerHTML += `
-          <div class="task">
-                  <input type="radio" id="task" name="task" value="task" class="input-radio">
-                  <label>${addTask.value}</label>
-                  <img src="https://img.icons8.com/ios-glyphs/512/multiply.png" class="delete-btn" id="delete-btn">
-              </div>
-      `;
+  console.log(myTasks);
 
+  for (i = 0; i < myTasks.length; i++) {
+    `
+    <div class="task">
+    <input type="radio" id="task" name="task" value="task" class="input-radio">
+    <label for="task">${myTasks[i]}</label>
+    <img src="https://img.icons8.com/ios-glyphs/512/multiply.png" class="delete-btn" id="delete-btn">
+    </div>
+    `;
+  }
+  incomplete.innerHTML += myTasks;
+  // console.log(myTasks);
 });
+
+// const incompleteTasks = [];
+
+// incomplete.addEventListener('click', function (e) {
+
+// });
